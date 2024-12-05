a14ff137dee0168ea2d37ba4ab81bf3bcbb34d86
Gen Vikat <gen.vikat@ametikool.ee>
2024-12-05 09:47:54 +0200

    Added form for adding new fruits and rerender the list, added delete for removing fruits

--- dom.js
+++ dom.js
@@ -7,25 +7,81 @@
 const h1element = document.querySelector('h1');
 h1element .innerText = "Head Teed!"
 
-
+// Näitame dokumendil listi puuviljadest
 const fruits = ["Apple", "Orange", "Banana", "Mango", "Kiwi"];
-
 const fruitListCointainer = document.querySelector('#fruit-list');
-const orderedListElement = document.createElement('ol');
 
-for(let i = 0; i < fruits.length; i++) {
-    const listElement = document.createElement('li');
-    listElement.innerText = fruits[i];
-    orderedListElement.appendChild(listElement);
+function renderFruitList() {
+    fruitListCointainer.innerHTML = "";
+    const orderedListElement = document.createElement('ol');
 
+    for(let i = 0; i < fruits.length; i++) {
+        const listElement = document.createElement('li');
+
+        const fruitNameSpanElement = document.createElement('span');
+        fruitNameSpanElement.textContent = fruits[i];
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = '🗑️';
+        deleteButton.setAttribute("data-index", i);
+        deleteButton.classList.add('delete-btn')
+
+        listElement.appendChild(fruitNameSpanElement);
+        listElement.appendChild(deleteButton);
+
+        orderedListElement.appendChild(listElement);
+    
+    }
+
+    fruitListCointainer.appendChild(orderedListElement);
 }
 
-fruitListCointainer.appendChild(orderedListElement);
+renderFruitList()
 
-
+// Lisame dokumenti nupu, mille üritusi saab kuulata
 const dontTouchMeButton = document.querySelector('#dont-touch-me-button');
 
 dontTouchMeButton.addEventListener('click', (event) => {
     console.log(event);
     alert("Ei tohtinud ju");
 });
+
+// Kuulame dokumendis olevat vormi ning lisame uue puuvilja
+const formForAddingNewFruits = document.querySelector("#add-new-fruit-form");
+console.log(formForAddingNewFruits);
+
+formForAddingNewFruits.addEventListener("submit", (event) => {
+    event.preventDefault();
+    console.log(event);
+
+    const formData = new FormData(event.target);
+    console.log(formData)
+
+    let fruitInputValue = formData.get("fruit");
+    console.log(fruitInputValue)
+
+    if (!fruitInputValue.trim()) {
+        alert("Fruit naming is needed.")
+        return;
+    };
+
+    fruits.push(fruitInputValue);
+    console.log(fruits)
+
+    renderFruitList()
+
+    event.target.reset();
+})
+
+fruitListCointainer.addEventListener("click", (event) => {
+    console.log(event)
+    const deleteButton = event.target.closest('.delete-btn');
+    console.log(deleteButton);
+
+    if (!deleteButton) return;
+
+    const index = parseInt(deleteButton.dataset.index);
+    fruits.splice(index, 1);
+    renderFruitList();
+})
+
